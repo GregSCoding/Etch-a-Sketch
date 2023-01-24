df2a869cc029de454dbdfc51496187e9f1838c43
GregSCoding <smendag@gmail.com>
2023-01-24 18:03:29 +0100

    Add sketching functionality

--- java.js
+++ java.js
@@ -1,10 +1,21 @@
 const container = document.querySelector("#gridcontainer");
+
+const drawingSquares = [];
 for(let i=0;i<16;i++){
   let row = document.createElement("div")
   container.appendChild(row);
   for(let j=0;j<16;j++){
-  let part = document.createElement("div")
-  part.classList.add("gridpart");
-  row.appendChild(part);
+    let part = document.createElement("div")
+    part.classList.add("gridpart");
+    row.appendChild(part);
+    drawingSquares.push(part);
   }
+}
+drawingSquares.forEach(function(elem){
+  elem.addEventListener("mouseover", colorSquare);
+})
+
+function colorSquare(event){
+  event.target.style.backgroundColor="rgb(100,100,100)";
+  return;
 }
